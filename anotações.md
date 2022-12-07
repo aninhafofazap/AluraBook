311ea6cf593376724cb4917c54c9c2e46ca6d1c3
Ana Victoria <anavictoriagomes1810@gmail.com>
2022-12-07 12:07:03 -0300

    Terminando a responsividade do destkop e ajustando alguns erros do swiper

--- anotações.md
+++ anotações.md
@@ -9,9 +9,4 @@ cinza: #474646;
 cinza-claro: #858585;
 
 
-## Observaçoes para corrigir
-
-Ajustar as imagens no tamanho correto do layout.
-Fazer com que a parte do e-mail fique no inicio do input com um padding-left de 2em no icone de email.
-
 
--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    spaceBetween: 10,
-    slidesPerview: 3,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    },
-});
+// Arquivo criado para execução do swiper, não foi usado.
+// O mesmo foi usado diretamente no body, arquivo HTML.
+
+
+
+// const swiper = new Swiper('.swiper', {
+//     spaceBetween: 10,
+//     slidesPerview: 3,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type: 'bullets',
+//     },
+// });
